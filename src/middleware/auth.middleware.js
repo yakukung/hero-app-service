a154cb47f9d11664c98655f440093a6511ce3024
Yakukung <yakukung2003@gmail.com>
2026-06-07 17:32:59 +0700

    feat: add account inactive check in auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,17 @@
 import { RESPONSE_MESSAGES } from "../constants/response.constant.js";
 import { HTTP_STATUS } from "../constants/http_status.constants.js";
+import { STATUS_FLAG } from "../constants/status_flag.constants.js";
 import { responseTemplates } from "../utils/response.utils.js";
 import { verifyAccessToken } from "../utils/jwt.utils.js";
+import { models } from "../models/sequelize/associations.js";
+
+const accountInactiveMessage = {
+  message: {
+    th: "บัญชีนี้ไม่สามารถใช้งานได้ โปรดติดต่อผู้ดูแลระบบ",
+    en: "This account cannot be used. Please contact the administrator.",
+  },
+};
+
 export const authMiddleware = async (req, res, next) => {
   try {
     const bearer = req.header("Authorization");
@@ -23,6 +33,19 @@ export const authMiddleware = async (req, res, next) => {
     switch (accessToken.code) {
       case HTTP_STATUS.OK.code:
         req.user = { id: accessToken.data.sub, role_id: accessToken.data.role };
+
+        const user = await models.Users.findOne({
+          where: { id: req.user.id },
+          attributes: ["status_flag"],
+        });
+
+        if (user && user.status_flag !== STATUS_FLAG.ACTIVE) {
+          const response = responseTemplates.setUnauthorizedResponse(
+            accountInactiveMessage,
+          );
+          return res.status(parseInt(response.code)).json(response);
+        }
+
         return next();
       case HTTP_STATUS.UNAUTHORIZED.code:
       case HTTP_STATUS.BAD_REQUEST.code:
